731ace3b7c0a38dd99f626becc7c624422115397
Trinkfass Lisa <Trinkfass.Lisa@sus.htl-grieskirchen.at>
2020-09-21 15:35:43 +0200

    Second change - implementierung abstract methods

--- ErastothenesPrimeSieve/src/erastothenesprimesieve/ErastothenesPrimeSieve.java
+++ ErastothenesPrimeSieve/src/erastothenesprimesieve/ErastothenesPrimeSieve.java
@@ -9,13 +9,36 @@ package erastothenesprimesieve;
  *
  * @author Lisa
  */
-public class ErastothenesPrimeSieve {
+public class ErastothenesPrimeSieve implements PrimeSieve {
 
-    /**
-     * @param args the command line arguments
-     */
+    int obergrenze = 0;
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        if (p <= 2) {
+            return (p == 2);
+        }
+        for (int i = 2; i * i <= p; i++) {
+            if (p % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void printPrimes() {
+        System.out.println("Primzahlen: ");
+        System.out.println("");
+        for (int i = 0; i < obergrenze; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + ", ");
+            }
+        }
     }
 
 }
