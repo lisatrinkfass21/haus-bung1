236ad6794de59a7fbf6601addca0eff49d3a7172
Trinkfass Lisa <Trinkfass.Lisa@sus.htl-grieskirchen.at>
2020-09-21 17:58:16 +0200

    third commit - konstrukter and  main

--- ErastothenesPrimeSieve/src/erastothenesprimesieve/ErastothenesPrimeSieve.java
+++ ErastothenesPrimeSieve/src/erastothenesprimesieve/ErastothenesPrimeSieve.java
@@ -5,15 +5,31 @@
  */
 package erastothenesprimesieve;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.TreeSet;
+
 /**
  *
  * @author Lisa
  */
 public class ErastothenesPrimeSieve implements PrimeSieve {
 
-    int obergrenze = 0;
+    ArrayList<Integer> listZahlen = new ArrayList<Integer>();
+    int obergrenze;
 
     public static void main(String[] args) {
+        System.out.println("Geben Sie eine Obergrenze ein");
+        Scanner scan = new Scanner(System.in, "Windows-1252");
+        int ober = Integer.parseInt(scan.nextLine());
+        ErastothenesPrimeSieve es = new ErastothenesPrimeSieve(ober);
+    }
+
+    public ErastothenesPrimeSieve(int obergrenze) {
+        this.obergrenze = obergrenze;
+        for (int i = 0; i <= obergrenze; i++) {
+            listZahlen.add(i);
+        }
 
     }
 
@@ -34,11 +50,10 @@ public class ErastothenesPrimeSieve implements PrimeSieve {
     public void printPrimes() {
         System.out.println("Primzahlen: ");
         System.out.println("");
-        for (int i = 0; i < obergrenze; i++) {
-            if (isPrime(i)) {
-                System.out.print(i + ", ");
-            }
+        for (int i = 0; i < listZahlen.size(); i++) {
+            System.out.print(listZahlen.get(i));
         }
+        
     }
 
 }
